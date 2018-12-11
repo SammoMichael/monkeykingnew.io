994e5afc29d4a8165aba8cca03c2cd1b5b6ca42c
Samuel Michael <samadammichael@yahoo.com>
2018-12-11 02:37:40 -0500

    detect touch

--- js/input.js
+++ js/input.js
@@ -1,9 +1,25 @@
-
-(function() {
-
+(() => {
     var pressedKeys = {};
-
+    
+    
     function setKey(event, status) {
+        var code = event.keyCode;
+        var key
+        switch (code) {
+        case 32:
+            key = 'SPACE'; break;
+        case 37:
+            key = 'LEFT'; break;
+        case 38:
+            key = 'UP'; break;
+        case 39:
+            key = 'RIGHT'; break;
+        case 40:
+            key = 'DOWN'; break;
+        default:
+            // Convert ASCII codes to letters
+            key = String.fromCharCode(code);
+        }
         console.log(event);
         console.log(status);
         // if window.touch = true
@@ -12,7 +28,6 @@
         // console.log(PointerEvent.type);
         // console.log(event.type);
         // console.log(event.MovementX);
-        var code = event.keyCode;
         // var pointerX = event.layerX;
         // var pointerY = event.layerY;
         // var playerX = window.player.sprite.pos[0];
@@ -23,45 +38,43 @@
         // console.log(event.layerY);
         
         if (event.type === "pointerdown") {
-            
-        if (event.path[0].id === "circle") {
-            key = 'SPACE';
-            status = true;
-            console.log('SPACE');
-            console.log(event.path);
-            console.log(event.path.id);
-            console.log(key);
+            if (event.path[0].id === "circle") {
+                key = 'SPACE';
+                // status = true;
+                console.log('SPACE');
+                console.log(event.path);
+                console.log(event.path.id);
+                console.log(key);
+            }
+            if (event.path[0].id === "circle2") {
+                key = 'UP';
+                // status = true;
+                console.log('UP');
+                console.log(key);
 
-        }
-        if (event.path[0].id === "circle2") {
-            key = 'UP';
-            status = true;
-            console.log('UP');
-            console.log(key);
+                console.log(event.path);
+            }
+            if (event.path[0].id === "circle3") {
+                // status = true;
+                key = 'RIGHT';
+                console.log('RIGHT');
+                console.log(key);
 
-            console.log(event.path);
+                // console.log('event');
+            }
+            if (event.path[0].id === "circle4") {
+                // status = true;
+                key = 'LEFT';
+                console.log('LEFT');
+                // console.log('event');
+            }
+            if (event.path[0].id === 'circle5') {
+                // status = true;
+                key = 'DOWN';
+                console.log('DOWN');
+                // console.log('event');
+            }
         }
-        if (event.path[0].id === "circle3") {
-            status = true;
-            key = 'RIGHT';
-            console.log('RIGHT');
-            console.log(key);
-
-            // console.log('event');
-        }
-        if (event.path[0].id === "circle4") {
-            status = true;
-            key = 'LEFT';
-            console.log('LEFT');
-            // console.log('event');
-        }
-        if (event.path[0].id === "circle5") {
-            status = true;
-            key = 'DOWN';
-            console.log('DOWN');
-            // console.log('event');
-        }
-    }
 
         // if (pointerX > playerX) {
         //     key = 'RIGHT';
@@ -83,21 +96,7 @@
         // if (event.PointerEvent.type === "pointerdown") {
         //     key = 'SPACE';
         // }
-        switch (code) {
-        case 32:
-            key = 'SPACE'; break;
-        case 37:
-            key = 'LEFT'; break;
-        case 38:
-            key = 'UP'; break;
-        case 39:
-            key = 'RIGHT'; break;
-        case 40:
-            key = 'DOWN'; break;
-        default:
-            // Convert ASCII codes to letters
-            key = String.fromCharCode(code);
-        }
+        
         // const fire = document.getElementById("joystick");
 
         //  fire.addEventListener('touchstart', (e) => { 
@@ -116,6 +115,9 @@
     document.addEventListener('pointerdown', (e) => {
         setKey(e, true);
     });
+    document.addEventListener('keydown', (e) => {
+        setKey(e, true);
+    });
     document.addEventListener('pointerup', (e) => {
         pressedKeys = {};
     });
@@ -130,17 +132,16 @@
     //     setKey(e, true);
     // });
 
-
-    document.addEventListener('keyup', function(e) {
+    document.addEventListener('keyup', (e) => {
         setKey(e, false);
     });
 
-    window.addEventListener('blur', function() {
+    window.addEventListener('blur', () => {
         pressedKeys = {};
     });
 
     window.input = {
-        isDown: function (key) {
+        isDown: (key) => {
             return pressedKeys[key.toUpperCase()];
         }
     };
